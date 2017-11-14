cd582d0da0100201daa93a2705023c62be81a5ff
Matt Faircloth <mattcfaircloth@gmail.com>
2017-11-14 22:25:17 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -84,8 +84,8 @@ end
 def get_average_age_for_season(data, season)
   age_total = 0
   num_of_contestants = 0
-  data[season].each do |contestant_hash|
-    age_total += (contestant_hash["age"]).to_i
+  data[season].each do |description|
+    age_total += (description["age"]).to_i
     num_of_contestants += 1
   end
   (age_total / num_of_contestants.to_f).round(0)
