fcbd1f8cef3d513f40b17d496a92ffb95fcb1d53
Matt Faircloth <mattcfaircloth@gmail.com>
2017-11-14 22:28:08 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,91 @@
+#require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
-end
+  data.each do |number, attributes|
+    attributes.each do |description|
+      description.each do |characteristic, value|
+        if number == season && value == "Winner"
+          return description["name"].split(" ").first
+        end
+    end
+   end
+  end
+ end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |number, attributes|
+    attributes.each do |description|
+      description.each do |characteristic, value|
+        if value == occupation
+          return description["name"]
+        end
+    end
+   end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |number, attributes|
+    attributes.each do |description|
+      description.each do |characteristic, value|
+        if value == hometown
+          counter += 1
+        end
+    end
+   end
+  end
+  return counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |number, attributes|
+    attributes.each do |description|
+      description.each do |characteristic, value|
+        if value == hometown
+          return description["occupation"]
+        end
+    end
+   end
+  end
 end
 
+# def get_average_age_for_season(data, season)
+#   average = []
+#   data.each do |number, attributes|
+#     attributes.each do |description|
+#       description.each do |characteristic, value|
+#         if number == season
+#           average << description["age"].to_i
+#         end
+#     end
+#    end
+#   end
+#   sum = 0
+#   average.each do |num|
+#     sum += num
+#  end
+#  sum.to_f.ceil / average.size
+# end
+
+# def get_average_age_for_season(data, season)
+#   age_total = 0
+#   num_of_contestants = 0
+#   data.each do |number, attributes|
+#       attributes.each do |description|
+#           age_total += (description["age"]).to_i
+#           num_of_contestants += 1
+#      end
+#     end
+#   (age_total / num_of_contestants.to_f).round(0)
+# end
+
 def get_average_age_for_season(data, season)
-  # code here
+  age_total = 0
+  num_of_contestants = 0
+  data[season].each do |description|
+    age_total += (description["age"]).to_i
+    num_of_contestants += 1
+  end
+  (age_total / num_of_contestants.to_f).round(0)
 end
