48490a4f95aeef1fcf3a4887c656cece355dbe7a
Matt Faircloth <mattcfaircloth@gmail.com>
2017-11-14 21:38:40 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -39,7 +39,15 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |number, attributes|
+    attributes.each do |description|
+      description.each do |characteristic, value|
+        if value == hometown
+          return description["occupation"].first
+        end
+    end
+   end
+  end
 end
 
 def get_average_age_for_season(data, season)
