973e8cf77b67a37eb775db5a90d919253baa5ba0
Matt Faircloth <mattcfaircloth@gmail.com>
2017-11-14 22:10:06 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -62,7 +62,7 @@ def get_average_age_for_season(data, season)
    end
   end
   # average.inject(0){|sum,x| sum + x } / average.length
-  return average.inject(average.inject(0) { |sum, x| sum += x }.to_f / average.size)
+  return average.inject(average.inject(0) { |sum, x| sum += x }.to_f / average.size).round(0)
   # return average.to_s
 
 end
